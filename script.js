e601b66577be4cba24e90d80e71ec0de2c515ad9
Isaac Cormier <ikcorm@iastate.edu>
2025-09-03 17:01:59 -0500

    new javascript file and function added (script.js), and the image for the battle of valmy now changes to the battle of the nile when clicked. also a new img file depicting the battle of the nile and the image sizes were standardized

---
+++ script.js
@@ -0,0 +1,11 @@
+function change_setting_box() {
+    change_setting_box_image();
+}
+
+function change_setting_box_image(){
+    document.getElementById("img_setting_box_french_revolutionary_wars_1792_1802").src="images/The_Battle_of_the_Nile.jpg";
+}
+
+function change_setting_box_caption(){
+
+}
